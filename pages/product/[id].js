7409078d26423262f72ce05662e1ab2bd97f1e65
unknown <janoncarlos@gmail.com>
2022-02-06 15:18:24 -0300

    second  commit

---
+++ pages/product/[id].js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+const ProductItem = () => {
+  const {
+    query: { id },
+  } = useRouter();
+  return (
+    <div>
+      <h1>PAGINA DE PRDUCTO : {id}</h1>
+    </div>
+  );
+};
+
+export default ProductItem;
